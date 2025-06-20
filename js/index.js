7a7589d3b4f9633c254b38342eec7e9eb97c4329
Martin <martkeu@web.de>
2025-06-20 11:34:47 +0200

    add switch answer functionality

--- js/index.js
+++ js/index.js
@@ -16,3 +16,26 @@ function switchBookmark(e) {
         e.target.classList.add('bookmark-disable');
     }
 }
+
+/*-----------------------------------------------------------------------------mk--
+| Switch Answer
+|----------------------------------------------------------------------------------
+| 
+*/
+const btnShowAnswer = document.querySelectorAll('.btnShowAnswer');
+const answerArea = document.querySelector('.answer-area');
+
+btnShowAnswer.forEach(btn => btn.addEventListener('click', switchAnswer));
+
+function switchAnswer(e) {
+    if (e.target.previousElementSibling.classList.contains('answer-hidden')) {
+        e.target.previousElementSibling.classList.remove('answer-hidden');
+        e.target.previousElementSibling.classList.add('answer-visible');
+        e.target.textContent = "Antwort ausblenden";
+    }
+    else {
+        e.target.previousElementSibling.classList.remove('answer-visible');
+        e.target.previousElementSibling.classList.add('answer-hidden');
+        e.target.textContent = "Antwort einblenden";
+    }
+}
